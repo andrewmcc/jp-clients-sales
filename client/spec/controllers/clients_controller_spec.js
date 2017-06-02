7b363f7885002e6095bc65602673f4092c89a6b2
andrewmcc <andrew.mccafferty@gmail.com>
2017-06-02 23:23:05 +0100

    Improved controller tests

--- client/spec/controllers/clients_controller_spec.js
+++ client/spec/controllers/clients_controller_spec.js
@@ -1,29 +1,75 @@
 describe('Clients Controller', () => {
 
+  let scope;
+  let deferred;
+  let $q;
+
+  let resolveSuccess = {
+    data: [{
+      "id": 1,
+      "firstname": "Elenor",
+      "surname": "White",
+      "company": "Mraz Inc",
+      "telephone": "1-984-375-2314 x762",
+      "country": "Venezuela"
+    }, {
+      "id": 2,
+      "firstname": "Donato",
+      "surname": "Sporer",
+      "company": "Kshlerin - Jerde",
+      "telephone": "161.991.2731",
+      "country": "Armenia"
+    }]
+  };
+
+  let resolveError = {
+    "status": 401,
+    "statusText": "Unauthorized",
+    "config": {
+      "url": "http://localhost:3000/fault"
+    }
+  };
+
   beforeEach(module('jpClientSales'));
 
-  let ClientsController
-  let scope;
-
-  beforeEach(inject(($controller, $rootScope) => {
-    scope = $rootScope.$new();
-    ClientsController = $controller('ClientsController', {
-      $scope: scope
-    });
-  }));
-
-  it('should call ClientService findAll()', inject(($q, ClientService) => {
-    var deferred = $q.defer();
+  beforeEach(inject(($controller, _$rootScope_, _$q_, ClientService) => {
+    $q = _$q_;
+    scope = _$rootScope_.$new();
+    deferred = _$q_.defer();
     spyOn(ClientService, 'findAll').and.returnValue(deferred.promise);
-    ClientsController.loadClients();
-    expect(ClientService.findAll).toHaveBeenCalled();
+    $controller('ClientsController as ctrl', { $scope: scope, ClientService: ClientService });
   }));
 
-  it('should call ClientService getHttpError()', inject(($q, ClientService) => {
-    var deferred = $q.defer();
-    spyOn(ClientService, 'getHttpError').and.returnValue(deferred.promise);
-    ClientsController.loadHttpError();
-    expect(ClientService.getHttpError).toHaveBeenCalled();
-  }));
+  it('should set clientList on promise resolution', () => {
+    deferred.resolve(resolveSuccess);
+    scope.$apply();
+    expect(scope.ctrl.clientList).not.toBe(undefined);
+  });
+
+  it('should receive two results on successful promise resolution', () => {
+    deferred.resolve(resolveSuccess);
+    scope.$apply();
+    expect(scope.ctrl.clientList.length).toEqual(2);
+  });
+
+  it('should not set httpError on promise resolution', () => {
+    deferred.resolve(resolveSuccess);
+    scope.$apply();
+    expect(scope.ctrl.httpError).toBe(undefined);
+  })
+
+  it('should set httpError on promise rejection', () => {
+    deferred.reject(resolveError);
+    scope.$apply();
+    expect(scope.ctrl.httpError).not.toBe(undefined);
+  });
+
+  it('should set correct httpError values', () => {
+    deferred.reject(resolveError);
+    scope.$apply();
+    expect(scope.ctrl.httpError.status).toBe(401);
+    expect(scope.ctrl.httpError.statusText).toBe('Unauthorized');
+    expect(scope.ctrl.httpError.config.url).toBe('http://localhost:3000/fault');
+  });
 
 });
--- client/spec/controllers/sales_controller_spec.js
+++ client/spec/controllers/sales_controller_spec.js
@@ -1,22 +1,79 @@
 describe('Sales Controller', () => {
 
+  let scope;
+  let deferred;
+  let $q;
+
+  let resolveSuccess = {
+    data: [{
+    	"id": "0001",
+    	"clientid": 4,
+    	"productname": "JavaScript & JQuery: Interactive Front-end Web Development",
+    	"price": "19.62",
+    	"size": 55
+    }, {
+    	"id": "0002",
+    	"clientid": 4,
+    	"productname": "JavaScript: The Good Parts",
+    	"price": "16.31",
+    	"size": 59
+    }, {
+    	"id": "0003",
+    	"clientid": 2,
+    	"productname": "JavaScript: The Ultimate Beginner's Guide!",
+    	"price": "12.99",
+    	"size": 93
+    }]
+  };
+
+  let resolveError = {
+    "status": 403,
+    "statusText": "Forbidden",
+    "config": {
+      "url": "http://localhost:3000/fault"
+    }
+  };
+
   beforeEach(module('jpClientSales'));
 
-  let SalesController
-  let scope;
-
-  beforeEach(inject(($controller, $rootScope) => {
-    scope = $rootScope.$new();
-    SalesController = $controller('SalesController', {
-      $scope: scope
-    });
-  }));
-
-  it('should call SalesService findAll()', inject(($q, SalesService) => {
-    var deferred = $q.defer();
+  beforeEach(inject(($controller, _$rootScope_, _$q_, SalesService) => {
+    $q = _$q_;
+    scope = _$rootScope_.$new();
+    deferred = _$q_.defer();
     spyOn(SalesService, 'findAll').and.returnValue(deferred.promise);
-    SalesController.loadSales();
-    expect(SalesService.findAll).toHaveBeenCalled();
+    $controller('SalesController as ctrl', { $scope: scope, SalesService: SalesService });
   }));
+
+  it('should set salesList on promise resolution', () => {
+    deferred.resolve(resolveSuccess);
+    scope.$apply();
+    expect(scope.ctrl.salesList).not.toBe(undefined);
+  });
+
+  it('should receive two results on successful promise resolution', () => {
+    deferred.resolve(resolveSuccess);
+    scope.$apply();
+    expect(scope.ctrl.salesList.length).toEqual(3);
+  });
+
+  it('should not set httpError on promise resolution', () => {
+    deferred.resolve(resolveSuccess);
+    scope.$apply();
+    expect(scope.ctrl.httpError).toBe(undefined);
+  })
+
+  it('should set httpError on promise rejection', () => {
+    deferred.reject(resolveError);
+    scope.$apply();
+    expect(scope.ctrl.httpError).not.toBe(undefined);
+  });
+
+  it('should set correct httpError values', () => {
+    deferred.reject(resolveError);
+    scope.$apply();
+    expect(scope.ctrl.httpError.status).toBe(403);
+    expect(scope.ctrl.httpError.statusText).toBe('Forbidden');
+    expect(scope.ctrl.httpError.config.url).toBe('http://localhost:3000/fault');
+  });
 
 });
